d0f14a2e98ef9408999fadb044b0923944909b6b
Stefan Berger <stefanb@linux.ibm.com>
2023-03-29 17:08:26 -0400

    phosphor: Activate IMA and EVM in init script
    Load IMA and EVM keys and activate EVM and load the IMA policy. When an
    IMA appraisal policy is loaded, prepare for running signed busybox and
    using signed libraries once the policy has been loaded. The signed
    libraries are needed during the final switch_root launching 'init'.
    
    Change-Id: I3cba35f6b7a8b5b3306a5d19daaa65fa185c7632
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- meta-phosphor/recipes-phosphor/initrdscripts/files/obmc-init.sh
+++ meta-phosphor/recipes-phosphor/initrdscripts/files/obmc-init.sh
@@ -126,6 +126,92 @@ try_wget() {
 	fi
 }
 
+# If securityfs is available and 'no_ima' is not found in $optfile then
+# - load IMA and EVM keys
+# - activate EVM if an EVM key was loaded
+# - load the IMA policy; in case an appraise policy is used adjust PATH so that
+#   signed executables from /root are used rather than the ones from the
+#   initrd
+#
+activate_ima_evm() {
+	if ! grep -w "securityfs" "/proc/filesystems" >/dev/null ||
+	     grep -w no_ima "$optfile" >/dev/null
+	then
+		return
+	fi
+
+	mount -t securityfs securityfs "/sys/kernel/security"
+
+	for kt in ima evm
+	do
+		if test -r "/root/etc/keys/x509_$kt.der"
+		then
+			keyctl padd asymmetric '' %keyring:.$kt \
+			    < "/root/etc/keys/x509_$kt.der" >/dev/null \
+			  && echo "Successfully loaded key onto .$kt keyring"
+		fi
+	done
+
+	# Activate EVM if .evm keyring exists and is not empty
+	if test -w "/sys/kernel/security/evm" -a \
+		-n "$(grep -s ' .evm:' "/proc/keys")" -a \
+		-z "$(grep -s ' .evm: empty' "/proc/keys")"
+	then
+		# EVM key loaded, activate it
+		evm_act=0x80000002
+		if echo "${evm_act}" > "/sys/kernel/security/evm"
+		then
+			printf "Activated EVM: $(cat "/sys/kernel/security/evm") [ activated with 0x%x ]\n" $evm_act
+		else
+			printf "Error: Failed to activate EVM with 0x%x\n" $evm_act
+		fi
+	fi
+
+	# Load IMA policy
+	ima_policy="/root/etc/ima/ima-policy"
+
+	if test -w "/sys/kernel/security/ima/policy" -a -r "$ima_policy"
+	then
+		load_ima_policy=false
+
+		# If a signed policy is required ...
+		if grep -q -E "^appraise func=POLICY_CHECK" "$ima_policy"
+		then
+			# ... check that .ima exists and is not empty
+			if test \
+			   -n "$(grep -s ' .ima:' "/proc/keys")" -a \
+			   -z "$(grep -s ' .ima: empty' "/proc/keys")"
+			then
+				load_ima_policy=true
+			else
+				echo "Error: Not loading IMA appraise policy since there is no key on .ima"
+			fi
+		else
+			# no signed policy: load it in any case
+			load_ima_policy=true
+		fi
+
+		if $load_ima_policy
+		then
+			# If an appraise policy is going to be activated then use signed
+			# busybox and libraries from /root
+			if grep -q \
+				-E "appraise .*func=(MMAP_CHECK|BPRM_CHECK)" \
+				"$ima_policy"
+			then
+				mount --bind "/root/bin/busybox.nosuid" \
+						/bin/busybox.nosuid
+				mount --bind "/root/lib/ld-linux-armhf.so.3" \
+						/lib/ld-linux-armhf.so.3
+				export LD_LIBRARY_PATH="/root/lib"
+			fi
+			if ! echo "$ima_policy" > "/sys/kernel/security/ima/policy"; then
+				echo "Error: Failed to load IMA policy"
+			fi
+		fi
+	fi
+}
+
 debug_takeover() {
 	echo "$@"
 
@@ -444,6 +530,8 @@ Change Root test failed!
 HERE
 	debug_takeover "$msg"
 done
+
+activate_ima_evm
 
 for f in $fslist
 do
