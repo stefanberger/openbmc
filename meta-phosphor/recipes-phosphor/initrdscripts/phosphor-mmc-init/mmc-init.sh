1795aaf1e65e528c6aed7b37c851f31bb3a60e24
Stefan Berger <stefanb@linux.ibm.com>
2023-05-31 22:41:28 -0400

    mmc-init: Activate IMA and EVM in init script
    Load IMA and EVM keys and activate EVM and load the IMA policy. When an
    IMA appraisal policy is loaded, prepare for running signed busybox and
    using signed libraries once the policy has been loaded. The signed
    libraries are needed during the final switch_root launching 'init'.
    
    Avoid activation of IMA and EVM if not running on QEMU and the secure
    boot is disabled (jumper on motherboard) to allow installation of
    (unsigned) tools for debugging.
    
    Change-Id: I9c950f0bd1686b1b5ca94d2e16ee4032e457f33c
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init/mmc-init.sh
+++ meta-phosphor/recipes-phosphor/initrdscripts/phosphor-mmc-init/mmc-init.sh
@@ -18,6 +18,102 @@ kgetopt ()
     [ -n "$_optval" ] && echo "$_optval"
 }
 
+# If securityfs is available then
+# - load IMA and EVM keys
+# - activate EVM if an EVM key was loaded
+# - load the IMA policy; in case an appraise policy is used adjust PATH so that
+#   signed executables from $rodir are used rather than the ones from the
+#   initrd
+#
+activate_ima_evm() {
+    if ! grep -w "securityfs" "/proc/filesystems" >/dev/null
+    then
+        return
+    fi
+
+    # If not running on QEMU: if secure boot is not enabled then also do not
+    # enable IMA & EVM
+    if test -z "$(dmesg | grep -i qemu)"
+    then
+        mount /sys/kernel/debug /sys/kernel/debug -t debugfs
+        sb=$(grep 1 /sys/kernel/debug/aspeed/sbc/secure_boot)
+        umount /sys/kernel/debug
+        if test -z "$sb"
+        then
+            return
+        fi
+    fi
+
+    mount -t securityfs securityfs /sys/kernel/security
+
+    for kt in ima evm
+    do
+        if test -r "$rodir/etc/keys/x509_$kt.der"
+        then
+            keyctl padd asymmetric '' %keyring:.$kt \
+                < "$rodir/etc/keys/x509_$kt.der" >/dev/null \
+            && echo "Successfully loaded key onto .$kt keyring"
+        fi
+    done
+
+    # Activate EVM if .evm keyring exists and is not empty
+    if test -w /sys/kernel/security/evm && \
+        grep -sq ' .evm:' /proc/keys && \
+        grep -sq ' .evm: empty' /proc/keys
+    then
+        # EVM key loaded, activate it
+        evm_act=0x80000002
+        if echo "$evm_act" > /sys/kernel/security/evm
+        then
+            printf "Activated EVM: $(cat /sys/kernel/security/evm) [ activated with 0x%x ]\n" $evm_act
+        else
+            printf "Error: Failed to activate EVM with 0x%x\n" $evm_act
+        fi
+    fi
+
+    # Load IMA policy
+    ima_policy="$rodir/etc/ima/ima-policy"
+
+    if test -w /sys/kernel/security/ima/policy -a -r "$ima_policy"
+    then
+        load_ima_policy=false
+
+        # If a signed policy is required ...
+        if grep -q -E "^appraise func=POLICY_CHECK" "$ima_policy"
+        then
+            # ... check that .ima exists and is not empty
+            if grep -sq ' .ima:' /proc/keys && \
+               grep -sq ' .ima: empty' /proc/keys
+            then
+                load_ima_policy=true
+            else
+                echo "Error: Not loading IMA appraise policy since there is no key on .ima"
+            fi
+        else
+            # no signed policy: load it in any case
+            load_ima_policy=true
+        fi
+
+        if $load_ima_policy
+        then
+            # If an appraise policy is going to be activated then use signed
+            # busybox and libraries from $rodir
+            if grep -q \
+                -E "appraise .*func=(MMAP_CHECK|BPRM_CHECK)" \
+                "$ima_policy"
+            then
+                mount --bind "$rodir/bin/busybox.nosuid" /bin/busybox.nosuid
+                mount --bind "$rodir/lib/ld-linux-armhf.so.3" \
+                        /lib/ld-linux-armhf.so.3
+                export LD_LIBRARY_PATH="$rodir/lib"
+            fi
+            if ! echo "$ima_policy" > /sys/kernel/security/ima/policy; then
+                echo "Error: Failed to load IMA policy"
+            fi
+        fi
+    fi
+}
+
 fslist="proc sys dev run"
 rodir=/mnt/rofs
 mmcdev="/dev/mmcblk0"
@@ -104,6 +200,8 @@ mkdir -p $rodir/var/persist/etc $rodir/var/persist/etc-work $rodir/var/persist/h
 mount overlay $rodir/etc -t overlay -o lowerdir=$rodir/etc,upperdir=$rodir/var/persist/etc,workdir=$rodir/var/persist/etc-work
 
 init="$(kgetopt init /sbin/init)"
+
+activate_ima_evm
 
 for f in $fslist; do
     mount --move "$f" "$rodir/$f"
